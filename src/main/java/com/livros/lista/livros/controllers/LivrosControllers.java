e368b4faf974a0e2dc6daa0876d3697327a7ba29
delivery.api <jadeiltondm@gmail.com>
2024-02-09 10:20:43 -0300

    feat: Update LivrosControllers

--- src/main/java/com/livros/lista/livros/controllers/LivrosControllers.java
+++ src/main/java/com/livros/lista/livros/controllers/LivrosControllers.java
@@ -1,15 +1,29 @@
 package com.livros.lista.livros.controllers;
 
 
+import com.livros.lista.livros.domain.Livros;
+import com.livros.lista.livros.service.LivrosServices;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/livros")
 public class LivrosControllers {
 
 
+    private final LivrosServices livrosServices;
 
+    public LivrosControllers(LivrosServices services){
+        this.livrosServices = services;
+    }
+    @GetMapping("")
+    public String getLivros(){
+        List<Livros> livros = this.livrosServices.getLivrosFromLibriary();
+        return "oiii";
+    }
 
 
 }
