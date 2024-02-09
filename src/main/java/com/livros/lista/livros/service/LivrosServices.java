f5c72249d5706bb7ad7898c5c44bce1ce1417e74
delivery.api <jadeiltondm@gmail.com>
2024-02-08 21:56:46 -0300

    feat: create LivrosServices

---
+++ src/main/java/com/livros/lista/livros/service/LivrosServices.java
@@ -0,0 +1,32 @@
+package com.livros.lista.livros.service;
+
+import com.livros.lista.livros.domain.Livros;
+import com.livros.lista.livros.feign.LivrosCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LivrosServices {
+
+
+    private final LivrosCliente livrosCliente;
+
+
+    public LivrosServices(LivrosCliente cliente){
+        this.livrosCliente = cliente;
+    }
+
+
+    public List<Livros> getLivrosFromLibriary(){
+        return this.livrosCliente.getLivros();
+
+
+
+    }
+
+
+
+
+}
